4706b97a4f5dc730bb3c2af19746c624e18c8417
traderjeffb <traderjeffb@gmail.com>
2021-08-07 12:00:30 -0400

    added details page

--- resources/views/employee/details.blade.php
+++ resources/views/employee/details.blade.php
@@ -1,10 +1,9 @@
-
+{{-- 
 @extends('layouts.app')
-
 @section('content')
 <div class="">
   <div class="d-flex">
-    <h3 class="mx-auto mt-4">New Employee Infomation</h3>
+    <h3 class="mx-auto mt-4">Employee Infomation</h3>
   </div>
   <div class="form d-flex ms-2">
     <form class="form-group form-control-md col-md-4 border p-1  mx-auto  bg-light font-weight-bold "  action="{{ url('employee.store') }}" method="post" id="add-employee-form">
@@ -49,4 +48,58 @@
     </form>
   </div>   
 </div>
+@endsection --}}
+
+
+
+
+
+
+
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex">  
+  <h3 class="mx-auto mt-4" >Details : {{ $employee['name'] }} </h3>
+</div>
+  <div class=" container d-flex">
+    <form class="form-group form-control-md col-md-4 border p-1  mx-auto  
+      bg-light font-weight-bold" id="add-employee-form">
+      @csrf
+            <label for="name" class="ml-md-2" >Name</label>
+            <input class="input-group" name="name" value="{{ $employee['name'] }}" readonly><br>
+            <label for="address" class="ml-md-2">Address</label>
+            <input class="input-group"  name="address" value="{{ $employee['address'] }}" readonly><br>
+            <label for="city" class="ml-md-2">City</label>
+            <input  class="input-group" name="city" value="{{ $employee['city'] }}" readonly><br>
+            <label for="state" class="ml-md-2">State</label>
+            <input  class="input-group" name="state" value="{{ $employee['state'] }}" readonly><br>
+            <label for="zipcode" class="ml-md-2">Zipcode</label>
+            <input  class="input-group" name="zipcode" value="{{ $employee['zipcode'] }}"readonly>
+
+            <label for="employment_status">Employment Status:</label><br>
+              <input type="radio" id="fullTime" name="employment_status" value="fullTime" {{ ($employee['employment_status']=="fullTime")? "checked" : "" }}>
+              <label for="fullTime">Full Time</label><br>
+              
+              <input type="radio" id="partTime" name="employment_status" value="partTime" {{ ($employee['employment_status']=="partTime")? "checked" : "" }}
+              >
+              <label for="partTime">Part Time</label><br>
+  
+              <input type="radio" id="tempHelp" name="employment_status" value="tempHelp" {{ ($employee['employment_status']=="tempHelp")? "checked" : "" }}>
+              <label for="tempHelp">Temporary Help</label><br>
+
+              <div>
+                <label for="gender">Gender:</label>
+                <select id="gender" name="gender">
+                  <option value="">--Please Make a Selection--</option>
+                  <option value="male" {{ ($employee['gender']=="male")? "selected" : "" }}>Male</option>
+                  <option value="female" {{ ($employee['gender']=="female")? "selected" : "" }}>Female</option>
+                  <option value="other" {{ ($employee['gender']=="other")? "selected" : "" }}>Other</option>
+                </select>
+              </div>
+            <button class= "m-2 rounded"><a class="nav-item nav-link active font-weight-bold" href= {{ route('employee.index') }}>Back</a></button>
+    </form>
+  </div>   
+</div>
 @endsection
+
