1ac76c2bd8f6164e536d662e509b2c6746e8aa60
traderjeffb <traderjeffb@gmail.com>
2021-08-08 12:00:30 -0400

    changed app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
+  
 {{-- <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head> --}}
@@ -12,30 +13,40 @@
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css
+">
+
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
 </head>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand fs-4 bold" href="#">Yorktown Machinery & Supplies</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
+        {{-- <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li> --}}
+        <li class="nav-item  m-2 p-1 fs-4">
+          <a href= {{ route('customer.index') }} class="text-decoration-none">Customer</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li class="nav-item m-2 p-1 fs-4">
+          <a href= {{ route('employee.index') }} class="text-decoration-none">Employees</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li class="nav-item m-2 p-1 fs-4">
+          <a href= {{ route('product.indexInternal') }} class="text-decoration-none">Sales</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item m-2 p-1 fs-4">
+          <a href= {{ route('mgt.index') }} class="text-decoration-none">Management</a>
+        </li>
+        {{-- <li class="nav-item p-1 fs-4">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+        </li> --}}
       </ul>
     </div>
   </div>
